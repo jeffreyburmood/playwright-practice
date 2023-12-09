0aa933f5c49ee945dd194395f903d5e588371471
Jeffrey Burmood <Jeffrey-swdev@proton.me>
2023-12-09 06:28:57 -0700

    lots of initial setup and config

--- test_example.py
+++ test_example.py
@@ -1,5 +1,5 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, sync_playwright, Playwright
 
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
@@ -15,3 +15,14 @@ def test_get_started_link(page: Page):
 
     # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+def test_browser_context_approach(playwright: Playwright):
+    webkit = playwright.webkit
+    browser = webkit.launch(headless=False)
+    # create a new incognito browser context
+    context = browser.new_context()
+    # create a new page inside context.
+    page = context.new_page()
+    page.goto("https://example.com")
+    # dispose context once it is no longer needed.
+    context.close()
